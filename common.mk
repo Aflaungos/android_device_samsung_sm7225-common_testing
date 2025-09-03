b8ed8bf0ef650251a352804f151614e86af8b583
Aflaungos <vimavick@gmail.com>
2025-09-03 16:43:27 -0300

    common.mk: Remove local sensors source

--- common.mk
+++ common.mk
@@ -379,11 +379,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    sensors.samsung \
-    libsensorndkbridge \
-    
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    libsensorndkbridge
 
 # Touch features
 PRODUCT_PACKAGES += \
