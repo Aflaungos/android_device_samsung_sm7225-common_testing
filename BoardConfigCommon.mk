25bb6bfdb69f79eb7656cad67f06f8646b94a82d
Aflaungos <vimavick@gmail.com>
2025-09-17 16:58:54 -0300

    BoardConfig: Fix wrong manifest path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,7 +192,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
-    $(COMMON_PATH)/vintf/manifest.xml
+    $(COMMON_PATH)/configs/manifest.xml
 
 DEVICE_MATRIX_FILE := \
     hardware/qcom-caf/common/compatibility_matrix.xml
