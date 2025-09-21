48f3e1d2a8b6fdade8156e1164096d71b877c193
Aflaungos <vimavick@gmail.com>
2025-09-21 13:29:52 -0300

    common.mk: Add mising separator

--- common.mk
+++ common.mk
@@ -342,7 +342,7 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+    libprotobuf-cpp-lite-vendorcompat \
     libsqlite.vendor
 
 # QTI fwk-detect
