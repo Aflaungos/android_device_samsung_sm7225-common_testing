bf4d9b67527b9db33a8811eabe8c8cd9a7d22b85
Aflaungos <vimavick@gmail.com>
2025-09-26 11:00:57 -0300

    sm7225-common: Switch to QTI Health and drop Lineage Health

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -183,13 +183,6 @@ TARGET_KEYMASTER_VARIANT := samsung
 # Lights
 $(call soong_config_set,samsungVars,target_specific_header_path,$(COMMON_PATH)/include)
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/batt_slate_mode
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED := 0
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED := 1
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
-
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
