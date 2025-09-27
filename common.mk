302b68e88ac2c214b83c1fe780b0d860ab86ffcb
Aflaungos <vimavick@gmail.com>
2025-09-27 11:19:15 -0300

    sm7225-common: Copy config.fs from sm7325

--- common.mk
+++ common.mk
@@ -203,8 +203,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.fastcharge@1.0-service.samsung
 
-# File System #TODO
-# TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
+# File System
+TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # FM
 PRODUCT_PACKAGES += \
