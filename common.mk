b0dd794013042fbe614240a02a70344b9510405d
Aflaungos <vimavick@gmail.com>
2025-09-26 21:57:46 -0300

    sm7225-common: Inherit emulated_storage.mk
    * Necessary for TWRP to mount data.

--- common.mk
+++ common.mk
@@ -450,6 +450,9 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
+# Storages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Touch features
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.sm7225
