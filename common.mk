e3de63758cf067487b38617fbc66710951b9edf6
Aflaungos <vimavick@gmail.com>
2025-09-29 23:20:00 -0300

    common.mk: Build libbatterylistener

--- common.mk
+++ common.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     libtinycompress \
+    libbatterylistener \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
