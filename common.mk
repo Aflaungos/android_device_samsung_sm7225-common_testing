412a1443625c0d4c0e33692f03183f244e9b5780
Aflaungos <vimavick@gmail.com>
2025-10-01 21:48:28 -0300

    common.mk: Add power.stats HAL

--- common.mk
+++ common.mk
@@ -411,6 +411,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# PowerStats
+PRODUCT_PACKAGES += \
+    libpixelpowerstats
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
