e3283c5198b72e66d2d31e0a6be9173431dac7eb
Simon1511 <simon2002.schoenmackers@gmail.com>
2025-11-17 18:57:41 -0300

    sm7225: Import libinit from samsung sm7125 and adapt to the sm7225 platform
    * Taken from https://github.com/LineageOS/android_device_samsung_sm7125-common/commits/lineage-23.0/init

---
+++ init/init_sm7225.h
@@ -0,0 +1,32 @@
+#ifndef INIT_SEC_H
+#define INIT_SEC_H
+
+#include <string.h>
+
+enum device_variant {
+    VARIANT_M236B = 0,
+    VARIANT_E236B,
+    VARIANT_MAX
+};
+
+typedef struct {
+    std::string model;
+    std::string codename;
+} variant;
+
+static const variant international_models_m23 = {
+    .model = "SM-M236B",
+    .codename = "m23xq"
+};
+
+static const variant india_models_f23 = {
+    .model = "SM-E236B",
+    .codename = "m23xq"
+};
+
+static const variant *all_variants[VARIANT_MAX] = {
+    &international_models_m23,
+    &india_models_f23,
+};
+
+#endif // INIT_SEC_H
