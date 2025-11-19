d0c8233a281276c6cf60216385df5713d4a4c57c
LuK1337 <priv.luk@gmail.com>
2025-11-19 11:58:08 -0300

    sm7225: Build android.hardware.bluetooth@1.0.vendor
    Required by Bluetooth stack.

--- common.mk
+++ common.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
