fb5881286e4abda6fcbbde1e34cd59dc15d9b1b3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2025-11-19 12:52:41 -0300

    sm7225: Build vendor interfaces libraries

--- common.mk
+++ common.mk
@@ -207,7 +207,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     libcrypto_shim.vendor \
     libdrmclearkeyplugin \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor
 
 # fastbootd
 PRODUCT_PACKAGES += \
