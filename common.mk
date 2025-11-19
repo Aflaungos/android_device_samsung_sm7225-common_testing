ae39a12021070c66788b98e12fabf5c1408b35cd
Andrzej Szafran <rtd1250@gmail.com>
2025-11-19 14:31:27 -0300

    sm7225: Build tetheroffload

--- common.mk
+++ common.mk
@@ -482,7 +482,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     libipanat \
     ipacm \
-    IPACM_cfg.xml
+    IPACM_cfg.xml \
+    android.hardware.tetheroffload.config@1.0.vendor  \
+    android.hardware.tetheroffload.control@1.0.vendor
 
 # Thermal
 PRODUCT_PACKAGES += \
