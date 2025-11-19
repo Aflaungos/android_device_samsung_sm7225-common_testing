4eca25719163b4d0061a81c59368507580ce8b73
Bruno Martins <bgcngm@gmail.com>
2025-11-19 11:07:38 -0300

    sm7225: Build opensource soundtrigger HAL

--- common.mk
+++ common.mk
@@ -85,6 +85,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl.samsung-sm7225 \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
+    sound_trigger.primary.lito \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default
