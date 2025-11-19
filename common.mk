5e90e249315e0ad7880927c7a3b061dc7a8aa1b4
Bruno Martins <bgcngm@gmail.com>
2025-11-19 14:28:06 -0300

    sm7225: Enable hardware tether offload support

--- common.mk
+++ common.mk
@@ -478,6 +478,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Tether
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor \
