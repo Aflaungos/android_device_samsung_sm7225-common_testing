fcb206d69d0662437b62985bd5a03dc66680d37c
Simon1511 <simon2002.schoenmackers@gmail.com>
2025-11-19 12:45:51 -0300

    sm7225: Build SamsungDoze from hardware/samsung

--- common.mk
+++ common.mk
@@ -210,6 +210,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/android.hardware.graphics.composer@2.4-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.graphics.composer@2.4-service.rc
 
+# Doze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey \
